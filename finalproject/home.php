d90a504d8bf85108ee1162097ef02aaa6f8beb2f
Jesse Castro <56277407+JesseCaztro@users.noreply.github.com>
2022-04-04 20:11:01 -0400

    Add files via upload
    i never clicked commit changes :/

---
+++ finalproject/home.php
@@ -0,0 +1,31 @@
+<?php
+// Get the 4 most recently added products
+$stmt = $pdo->prepare('SELECT * FROM products');
+$stmt->execute();
+$recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<?=template_header('Home')?>
+
+<div class="featured">
+    <h2>SpeakEasy</h2>
+    <p>Something to calm your nerves</p>
+</div>
+<div class="recentlyadded content-wrapper">
+    <h2>Recently Added Products</h2>
+    <div class="products">
+        <?php foreach ($recently_added_products as $product): ?>
+        <a href="index.php?page=product&id=<?=$product['id']?>" class="product">
+            <img src="imgs/<?=$product['img']?>" width="300" height="400" alt="<?=$product['name']?>">
+            <span class="name"><?=$product['name']?></span>
+            <span class="price">
+                &dollar;<?=$product['price']?>
+                <?php if ($product['rrp'] > 0): ?>
+                <span class="rrp">&dollar;<?=$product['rrp']?></span>
+                <?php endif; ?>
+            </span>
+        </a>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<?=template_footer()?>
