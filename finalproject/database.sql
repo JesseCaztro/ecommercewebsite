e9b76ba4b833779a3ff2b2f652d7de6d97e81dcb
Saba Syed <82114660+sabasyed1@users.noreply.github.com>
2022-04-20 13:10:21 -0400

    Update database.sql

--- finalproject/database.sql
+++ finalproject/database.sql
@@ -25,4 +25,148 @@ INSERT INTO `product` (`productId`, `name`, `price`, `madeDate`) VALUES
 (2, 'blank', 6, '2022-04-17 21:20:58'),
 (3, 'blank', 6, '2022-04-17 21:22:07'),
 (4, 'blank', 6, '2022-04-17 21:23:05');
+-- --------------------------------------------
 
+--
+-- Table structure for table `orderitems`
+--
+
+CREATE TABLE `orderitems` (
+  `id` int(21) NOT NULL,
+  `orderId` int(21) NOT NULL,
+  `productId` int(21) NOT NULL,
+  `itemQuantity` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderId` int(21) NOT NULL,
+  `userId` int(21) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `zipCode` int(21) NOT NULL,
+  `phoneNumber` bigint(21) NOT NULL,
+  `amount` decimal(4.2) NOT NULL,
+  `paymentMode` enum('0','1') NOT NULL DEFAULT '0' COMMENT '0=cash on delivery, \r\n1=online ',
+  `orderStatus` enum('0','1','2','3','4','5','6') NOT NULL DEFAULT '0' COMMENT '0=Order Placed.\r\n1=Order Confirmed.\r\n2=Preparing your Order.\r\n3=Your order is on the way!\r\n4=Order Delivered.\r\n5=Order Denied.\r\n6=Order Cancelled.',
+  `orderDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(21) NOT NULL,
+  `username` varchar(21) NOT NULL,
+  `firstName` varchar(21) NOT NULL,
+  `lastName` varchar(21) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `userType` enum('0','1') NOT NULL DEFAULT '0' COMMENT '0=user\r\n1=admin',
+  `password` varchar(255) NOT NULL,
+  `joinDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `email`, `phone`, `userType`, `password`, `joinDate`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin@gmail.com', 1111111111, '1', 'admin', '2021-04-11 11:40:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `viewcart`
+--
+
+CREATE TABLE `viewcart` (
+  `cartItemId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `itemQuantity` int(100) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `addedDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+
+
+--
+-- Indexes for dumped tables
+--
+
+
+--
+-- Indexes for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderId`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`prouctId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `viewcart`
+--
+ALTER TABLE `viewcart`
+  ADD PRIMARY KEY (`cartItemId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orderitems`
+--
+ALTER TABLE `orderitems`
+  MODIFY `id` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderId` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `productId` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(21) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `viewcart`
+--
+ALTER TABLE `viewcart`
+  MODIFY `cartItemId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
