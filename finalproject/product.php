2d5362a900d35dbdb1ea466990efae857a42335d
Jesse Castro <56277407+JesseCaztro@users.noreply.github.com>
2022-04-27 01:05:14 -0400

    Update product.php

--- finalproject/product.php
+++ finalproject/product.php
@@ -1,13 +1,13 @@
 <?php
 // Check to make sure the id parameter is specified in the URL
-if (isset($_GET['productId'])) {
+if (isset($_GET['id'])) {
     // Prepare statement and execute, prevents SQL injection
     $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
-    $stmt->execute([$_GET['productId']]);
+    $stmt->execute([$_GET['id']]);
     // Fetch the product from the database and return the result as an Array
-    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    $products = $stmt->fetch(PDO::FETCH_ASSOC);
     // Check if the product exists (array is not empty)
-    if (!$product) {
+    if (!$products) {
         // Simple error to display if the id for the product doesn't exists (array is empty)
         exit('Product does not exist!');
     }
