87df1d01835930ed07a75ec03a61fd58dbefbbbf
Jesse Castro <56277407+JesseCaztro@users.noreply.github.com>
2022-04-27 01:10:39 -0400

    Update cart.php

--- finalproject/cart.php
+++ finalproject/cart.php
@@ -1,35 +1,35 @@
 <?php
 // If the user clicked the add to cart button on the product page we can check for the form data
-if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity'])) {
+if (isset($_POST['id'], $_POST['quantity']) && is_numeric($_POST['id']) && is_numeric($_POST['quantity'])) {
     // Set the post variables so we easily identify them, also make sure they are integer
-    $product_id = (int)$_POST['product_id'];
+    $product_id = (int)$_POST['id'];
     $quantity = (int)$_POST['quantity'];
     // Prepare the SQL statement, we basically are checking if the product exists in our databaser
     $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
-    $stmt->execute([$_POST['product_id']]);
+    $stmt->execute([$_POST['id']]);
     // Fetch the product from the database and return the result as an Array
     $product = $stmt->fetch(PDO::FETCH_ASSOC);
     // Check if the product exists (array is not empty)
-    if ($product && $quantity > 0) {
+    if ($products && $quantity > 0) {
         // Product exists in database, now we can create/update the session variable for the cart
         if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
-            if (array_key_exists($product_id, $_SESSION['cart'])) {
+            if (array_key_exists($id, $_SESSION['cart'])) {
                 // Product exists in cart so just update the quanity
-                $_SESSION['cart'][$product_id] += $quantity;
+                $_SESSION['cart'][$id] += $quantity;
             } else {
                 // Product is not in cart so add it
-                $_SESSION['cart'][$product_id] = $quantity;
+                $_SESSION['cart'][$id] = $quantity;
             }
         } else {
             // There are no products in cart, this will add the first product to cart
-            $_SESSION['cart'] = array($product_id => $quantity);
+            $_SESSION['cart'] = array($id => $quantity);
         }
     }
     // Prevent form resubmission...
     header('location: index.php?page=cart');
     exit;
 }
-// Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
+// Remove product from cart, check for the URL param "remove", this is the product's id, make sure it's a number and check if it's in the cart
 if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
     // Remove the product from the shopping cart
     unset($_SESSION['cart'][$_GET['remove']]);
