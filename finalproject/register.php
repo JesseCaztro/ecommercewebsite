2c237c4829b6f6c0dc9fd6c2c45f5d2ea2844405
Saba Syed <82114660+sabasyed1@users.noreply.github.com>
2022-05-04 15:48:40 -0400

    Create register.php

---
+++ finalproject/register.php
@@ -0,0 +1,27 @@
+<?php
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <p>
+        Thank you <?php echo $email;?> your password is <?php echo $password;?> 
+        <br>
+        
+    </p>
+  
+</body>
+
+</html>
