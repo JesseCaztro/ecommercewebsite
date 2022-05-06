ec18c1608610e6d06e78c29140a33a9d22d54ea9
Saba Syed <82114660+sabasyed1@users.noreply.github.com>
2022-05-05 23:28:57 -0400

    Add files via upload

---
+++ Liq/functions.php
@@ -0,0 +1,91 @@
+<?php
+function pdo_connect_mysql() {
+    // Update the details below with your MySQL details
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $dsn = 'mysql:dbname=liqourland;host=localhost';
+    try {
+    	return new PDO($dsn, $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	exit('Failed to connect to database!');
+    }
+}
+// Template header, feel free to customize this
+function template_header($title) {
+
+// Get the amount of items in the shopping cart, this will be displayed in the header.
+$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
+
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+            <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,700;0,800;1,200;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+        <header>
+            <div class="content-wrapper">
+                <h1>Liquorland CO.</h1>
+                <nav>
+                    <a href="index.php">Home</a>
+                    <a href="index.php?page=products">Products</a>
+                    <a href="account.php">Account</a>
+                    <a href="search.php">Search</a>
+                </nav>
+                <div class="link-icons">
+                    <a href="index.php?page=cart">
+						<i class="fas fa-shopping-cart"></i>
+						<span>$num_items_in_cart</span>
+					</a>
+                </div>
+            </div>
+        </header>
+        <main>
+EOT;
+}
+// Template footer
+function template_footer() {
+$year = date('Y');
+echo <<<EOT
+        </main>
+        <footer>
+            <div class="content-wrapper">
+                <p>&copy; $year, Liquorland CO.</p>
+            </div>
+        </footer>
+          <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+        <script src="script.js"></script>
+    </body>
+</html>
+EOT;
+}
+?>
